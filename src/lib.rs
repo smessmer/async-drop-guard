f8d363687cfe7b4268835abfb1ea96da61260e76
Sebastian Messmer <messmer@cryfs.org>
2022-07-17 14:41:02 -0700

    Rename modules to fix build

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-mod async_drop_trait;
-pub use async_drop_trait::AsyncDrop;
+mod async_drop;
+pub use async_drop::AsyncDrop;
 
-mod async_drop_guard;
-pub use async_drop_guard::AsyncDropGuard;
+mod guard;
+pub use guard::AsyncDropGuard;
 
 mod async_drop_arc;
 pub use async_drop_arc::AsyncDropArc;
