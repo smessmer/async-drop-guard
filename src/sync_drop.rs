0ab5e1ef518eff44de8a2b42270978c9ad8bcea1
Sebastian Messmer <messmer@cryfs.org>
2022-07-17 18:04:16 -0700

    Avoid double panic in SyncDrop

--- src/sync_drop.rs
+++ src/sync_drop.rs
@@ -1,5 +1,6 @@
 use std::fmt::Debug;
 use std::ops::{Deref, DerefMut};
+use std::panic::UnwindSafe;
 
 use super::{AsyncDrop, AsyncDropGuard};
 
@@ -7,9 +8,9 @@ use super::{AsyncDrop, AsyncDropGuard};
 /// synchronous [Drop] destructor.
 ///
 /// WARNING: This can cause deadlocks, see https://stackoverflow.com/questions/71541765/rust-async-drop
-pub struct SyncDrop<T: Debug + AsyncDrop>(Option<AsyncDropGuard<T>>);
+pub struct SyncDrop<T: Debug + AsyncDrop + UnwindSafe>(Option<AsyncDropGuard<T>>);
 
-impl<T: Debug + AsyncDrop> SyncDrop<T> {
+impl<T: Debug + AsyncDrop + UnwindSafe> SyncDrop<T> {
     pub fn new(v: AsyncDropGuard<T>) -> Self {
         Self(Some(v))
     }
@@ -23,22 +24,33 @@ impl<T: Debug + AsyncDrop> SyncDrop<T> {
     }
 }
 
-impl<T: Debug + AsyncDrop> Drop for SyncDrop<T> {
+impl<T: Debug + AsyncDrop + UnwindSafe> Drop for SyncDrop<T> {
     fn drop(&mut self) {
         if let Some(mut v) = self.0.take() {
-            futures::executor::block_on(v.async_drop()).unwrap();
+            if std::thread::panicking() {
+                // If we're dropping this because of a panic, we want to avoid causing a double
+                // panic because they don't show any error message or backtrace and are super hard
+                // to debug. Instead, log the inner panic and let the outer panic continue.
+                if let Err(panic) = std::panic::catch_unwind(move || {
+                    futures::executor::block_on(v.async_drop()).unwrap()
+                }) {
+                    log::error!("Double panic.\nInner panic: {:?}", panic);
+                }
+            } else {
+                futures::executor::block_on(v.async_drop()).unwrap()
+            }
         }
     }
 }
 
-impl<T: Debug + AsyncDrop> Deref for SyncDrop<T> {
+impl<T: Debug + AsyncDrop + UnwindSafe> Deref for SyncDrop<T> {
     type Target = T;
     fn deref(&self) -> &T {
         self.0.as_ref().expect("Already dropped")
     }
 }
 
-impl<T: Debug + AsyncDrop> DerefMut for SyncDrop<T> {
+impl<T: Debug + AsyncDrop + UnwindSafe> DerefMut for SyncDrop<T> {
     fn deref_mut(&mut self) -> &mut T {
         self.0.as_mut().expect("Already dropped")
     }
