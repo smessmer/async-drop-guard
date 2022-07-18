12a770f350da7996f1349d86bb98896fc9773cd8
Sebastian Messmer <messmer@cryfs.org>
2022-07-17 18:09:20 -0700

    Don't require UnwindSafe in SyncDrop

--- src/sync_drop.rs
+++ src/sync_drop.rs
@@ -1,6 +1,6 @@
 use std::fmt::Debug;
 use std::ops::{Deref, DerefMut};
-use std::panic::UnwindSafe;
+use std::panic::{AssertUnwindSafe, UnwindSafe};
 
 use super::{AsyncDrop, AsyncDropGuard};
 
@@ -8,9 +8,9 @@ use super::{AsyncDrop, AsyncDropGuard};
 /// synchronous [Drop] destructor.
 ///
 /// WARNING: This can cause deadlocks, see https://stackoverflow.com/questions/71541765/rust-async-drop
-pub struct SyncDrop<T: Debug + AsyncDrop + UnwindSafe>(Option<AsyncDropGuard<T>>);
+pub struct SyncDrop<T: Debug + AsyncDrop>(Option<AsyncDropGuard<T>>);
 
-impl<T: Debug + AsyncDrop + UnwindSafe> SyncDrop<T> {
+impl<T: Debug + AsyncDrop> SyncDrop<T> {
     pub fn new(v: AsyncDropGuard<T>) -> Self {
         Self(Some(v))
     }
@@ -24,10 +24,13 @@ impl<T: Debug + AsyncDrop + UnwindSafe> SyncDrop<T> {
     }
 }
 
-impl<T: Debug + AsyncDrop + UnwindSafe> Drop for SyncDrop<T> {
+impl<T: Debug + AsyncDrop> Drop for SyncDrop<T> {
     fn drop(&mut self) {
         if let Some(mut v) = self.0.take() {
             if std::thread::panicking() {
+                // v is unwind safe because we're destroying it and not using it anymore after the async_drop call
+                // TODO Is this actually true? What if its Drop implementation relies on state?
+                let mut v = AssertUnwindSafe(v);
                 // If we're dropping this because of a panic, we want to avoid causing a double
                 // panic because they don't show any error message or backtrace and are super hard
                 // to debug. Instead, log the inner panic and let the outer panic continue.
